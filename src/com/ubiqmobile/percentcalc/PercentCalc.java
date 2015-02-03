9ace363add60b036cc885095dfc3943f2ce1c776
NamelessOne <nlessone@gmail.com>
2015-02-03 15:18:57 +0400

    Add base classes.

---
+++ src/com/ubiqmobile/percentcalc/PercentCalc.java
@@ -0,0 +1,20 @@
+package com.ubiqmobile.percentcalc;
+
+/**
+ * Created by NamelessOne
+ * 03.02.2015.
+ */
+public class PercentCalc {
+    private static final double THRESHOLD_STRETCH = 0.8;
+    private static final double THRESHOLD_TIME = 0.01;
+
+    private static final double AMPLITUDE_MAN = 1.1;
+    private static final double AMPLITUDE_MAX = 1.6;
+
+    private static final double DURATION_MIN = 0.03;
+    private static final double DURATION_MAX = 0.11;
+
+    private static final int L = 6;
+
+
+}
---
+++ src/com/ubiqmobile/percentcalc/ReadAcc.java
@@ -0,0 +1,12 @@
+package com.ubiqmobile.percentcalc;
+
+/**
+ * Created by NamelessOne
+ * 03.02.2015.
+ */
+public class ReadAcc {
+    //public static final String FILENAME = "AccTest_Dec.csv";
+    //public static final String FILENAME = "AccTestbefore_Dec.csv";
+    //public static final String FILENAME = "AccTest_Nov.csv";
+    public static final String FILENAME = "AccTestbefore_Dec.csv";
+}
